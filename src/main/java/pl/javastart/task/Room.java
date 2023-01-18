b3f5f88b90bcef6f12432185e00e12564782f846
Alina Burnos <burnosalina@gmail.com>
2023-01-18 10:09:41 +0100

    Klimatyzacja

---
+++ src/main/java/pl/javastart/task/Room.java
@@ -0,0 +1,61 @@
+package pl.javastart.task;
+
+public class Room {
+    private int squareMeters;
+    private double temperature;
+    private boolean airConditioner;
+    private double limitTemperature;
+
+    Room(int squareMeters, double temperature, boolean airConditioner, double limitTemperature) {
+        this.squareMeters = squareMeters;
+        this.temperature = temperature;
+        this.airConditioner = airConditioner;
+        this.limitTemperature = limitTemperature;
+    }
+
+    boolean lowerTemperature() {
+        if (airConditioner) {
+            if (temperature >= limitTemperature + 1) {
+                temperature -= 1;
+                return true;
+            } else {
+                temperature = limitTemperature;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    int getSquareMeters() {
+        return squareMeters;
+    }
+
+    void setSquareMeters(int squareMeters) {
+        this.squareMeters = squareMeters;
+    }
+
+    double getTemperature() {
+        return temperature;
+    }
+
+    void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    boolean isAirConditioner() {
+        return airConditioner;
+    }
+
+    void setAirConditioner(boolean airConditioner) {
+        this.airConditioner = airConditioner;
+    }
+
+    public double getLimitTemperature() {
+        return limitTemperature;
+    }
+
+    public void setLimitTemperature(double limitTemperature) {
+        this.limitTemperature = limitTemperature;
+    }
+}
